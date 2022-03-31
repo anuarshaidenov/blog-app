e3e6d0c68a710f2d502bca33405167cd611e1227
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-03-31 15:44:15 +0600

    Add tests for GET /users path

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,8 +6,16 @@ RSpec.describe "Users", type: :request do
       get '/users'
     end
 
+    it "handles GET requests" do
+      expect(response).to have_http_status(:ok)
+    end
+
     it "renders a correct template" do
       expect(response).to render_template(:index)
+    end
+
+    it "renders content correctly" do
+      expect(response.body).to include('<h1>Here you will find the list of users</h1>')
     end
   end
 end
