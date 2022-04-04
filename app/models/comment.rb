3bc24b619f3e4be124b82118a0360729dce3d21f
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-05 01:05:52 +0600

    Add increment counter methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  def increment_comment_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  def increment_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
