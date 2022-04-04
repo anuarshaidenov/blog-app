c1cce5eed12325f3f0fd91b63cb5f0bb5132fcb6
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-05 00:55:09 +0600

    Add increment_post_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  def increment_post_counter
+    author.increment!(:posts_counter)
+  end
 end
