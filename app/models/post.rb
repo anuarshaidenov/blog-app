a352527bc3b77ccecbbf809dd8a737057fbb9e7a
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-05 01:06:47 +0600

    Fix naming

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
 
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
     author.increment!(:posts_counter)
   end
 
-  def get_recent_comments
+  def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  def get_recent_posts
+  def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
 end
