0f26cf78aa4d4ad5bfbb3ccf370e96749a11e6a9
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-06 13:59:44 +0600

    Refactor models and create post-preview partial

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
+  after_save :increment_comment_counter
+
+  private
+
   def increment_comment_counter
     post.increment!(:comments_counter)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,10 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
+  after_save :increment_likes_counter
+
+  private
+
   def increment_likes_counter
     post.increment!(:likes_counter)
   end
