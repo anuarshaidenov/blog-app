7a9a82645a8e71ac885d7aa62cd72e82f67c17d6
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-07 15:41:36 +0600

    Add and style comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,12 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = Comment.new(post_params)
-    @comment.author = current_user
-    @comment.post = @post
+    @comment = Comment.new(text: params[:text], author: current_user, post: @post)
 
     if @comment.save
-      redirect_to user_post_path(user_id: @post.author.id, post_id: @post.id)
+      redirect_to user_post_path(user_id: @post.author.id, id: @post.id)
     else
       render user_post_path(user_id: @post.author.id, post_id: @post.id), alert: 'An error has occurred while creating the comment.'
     end
-  end
-
-  private
-
-  def post_params
-    params.require(:comment).permit(:text)
   end
 end
