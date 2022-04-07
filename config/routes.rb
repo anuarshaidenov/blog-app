4fc47408cd0bb5b73880a6f893141acf9b984349
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-07 15:46:59 +0600

    Add a structure for likes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create]
+      resources :likes, only: [:create]
     end
   end
 
