dea179d794013c8117ccb2b1577ad0a3520e9024
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-13 01:32:43 +0600

    Add destroy method to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments, :likes)
@@ -21,6 +23,14 @@ class PostsController < ApplicationController
     else
       render :new, alert: 'An error has occurred while creating the post'
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @author = @post.author
+    @author.posts_counter -= 1
+    @post.destroy!
+    redirect_to user_posts_path(id: @author.id), notice: 'Post was deleted successfully!'
   end
 
   private
