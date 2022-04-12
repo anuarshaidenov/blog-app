e2fad9b4c95789da18a6d9a0ebdb3785e97ccee7
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-13 01:34:38 +0600

    Add route to destroy post

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
     end
