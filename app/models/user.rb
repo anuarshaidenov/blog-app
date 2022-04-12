ee645de452724ba2ce172750281c1f495989a22e
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-13 01:34:08 +0600

    Add admin? method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def admin?
+    role == 'admin'
+  end
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
