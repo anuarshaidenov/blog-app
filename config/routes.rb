1575a72786855f738a1763073c170e174162b2b1
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-13 01:44:21 +0600

    Add route to destroy comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:create]
+      resources :comments, only: [:create, :destroy]
       resources :likes, only: [:create]
     end
   end
