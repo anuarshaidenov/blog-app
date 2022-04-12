02931dcc8d5f5cb2017822fbb98fb6297bffc863
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-13 01:33:19 +0600

    Configure abilities to delete post

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,14 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, Post, author_id: user.id
+      can :read, :all
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
