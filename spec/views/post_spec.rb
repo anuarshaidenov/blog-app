9941cae689e17367d911759cf2b53909a942def5
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-14 18:06:09 +0600

    Add test cases for post show view

--- spec/views/post_spec.rb
+++ spec/views/post_spec.rb
@@ -54,4 +54,45 @@ RSpec.describe 'Testing posts views', type: :feature do
       expect(current_path).to eq user_post_path user_id: User.last.id, id: Post.first.id 
     end
   end
+
+  describe "post#show views" do
+    before(:each) do
+      first_user = User.create!(name: 'Tom', photo: 'photo.jpg', bio: 'Teacher from Mexico.', email: 'to@example.com', password: 'password', confirmed_at: Time.now)
+      (1..5).each { |i| first_user.posts.create title: "Post number #{i}", text: "This is my #{i} post!" }
+      Comment.create! author: first_user, post: Post.last, text: "Hi, tom!"
+      Like.create! author: first_user, post: Post.last
+      visit user_session_path
+      fill_in "Email",	with: "to@example.com"
+      fill_in "Password",	with: "password"
+      click_on "Log in"
+      visit user_post_path user_id: first_user.id, id: Post.last.id
+    end
+
+    it "I can see the post's title." do
+      expect(page).to have_content "Post number 5"
+    end
+    
+    it "I can see who wrote the post." do
+      expect(page).to have_content "by Tom"
+    end
+    
+    it "I can see how many comments it has." do
+      expect(page).to have_content "Comments (1):"
+    end
+    
+    it "I can see how many likes it has." do
+      expect(page).to have_content "Likes: 1"
+    end
+
+    it "I can see the post body." do
+      expect(page).to have_content "This is my 5 post!"
+    end
+
+    it "I can see the username of each commentor." do
+      expect(page).to have_content "Tom:"
+    end
+    it "I can see the comment each commentor left" do
+      expect(page).to have_content "Hi, tom!"
+    end
+  end
 end
