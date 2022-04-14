034ec4f8413c145b3b3a7fa61f5391e0677de399
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-14 14:50:54 +0600

    Add login test case 4

--- spec/views/login_spec.rb
+++ spec/views/login_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Login page', type: :feature do
   before(:each) do
-    first_user = User.create!(name: 'Tom', photo: 'photo.jpg', bio: 'Teacher from Mexico.', email: 'to@example.com', password: 'password')
+    first_user = User.create!(name: 'Tom', photo: 'photo.jpg', bio: 'Teacher from Mexico.', email: 'to@example.com', password: 'password', confirmed_at: Time.now)
     visit user_session_path
   end
 
@@ -22,6 +22,13 @@ RSpec.describe 'Login page', type: :feature do
     fill_in "Password",	with: "sometext" 
     click_on 'Log in'
     expect(page).to have_content 'Invalid Email or password'
+  end
+  
+  it "When I click the submit button after filling in the username and the password with correct data, I am redirected to the root page." do
+    fill_in "Email",	with: "to@example.com"
+    fill_in "Password",	with: "password" 
+    click_on 'Log in'
+    expect(current_path).to eq root_path
   end
 
 end
