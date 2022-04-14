93a1bc311db5311a188b717f1a907529fb4847c9
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-14 19:08:30 +0600

    Change test describe to context

--- spec/views/user_spec.rb
+++ spec/views/user_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe 'Testing users views', type: :feature do
-  describe 'user#index views' do
+  context 'user#index views' do
     before(:each) do
       User.create!(name: 'Tom', photo: 'photo.jpg', bio: 'Teacher from Mexico.', email: 'to@example.com',
                    password: 'password', confirmed_at: Time.now)
@@ -27,7 +27,7 @@ RSpec.describe 'Testing users views', type: :feature do
     end
   end
 
-  describe 'users#show views' do
+  context 'users#show views' do
     before(:each) do
       first_user = User.create!(name: 'Tom', photo: 'photo.jpg', bio: 'Teacher from Mexico.', email: 'to@example.com',
                                 password: 'password', confirmed_at: Time.now)
