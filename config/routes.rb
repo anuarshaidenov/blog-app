4d55539dd39b603b48ba9d93ae062ab40bab6a1b
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2022-04-14 17:04:33 +0300

    Update: routes for api end points

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
+  # Define endpoints for
+  post '/api/login', to: 'authentication#login'
+  get '/api/posts', to: 'posts#get_posts'
+  get '/api/comments', to: 'comments#get_comments'
+  post '/api/comments', to: 'comments#add_comment'
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:create, :destroy]
