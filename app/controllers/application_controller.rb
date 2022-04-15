f0e2dda067f723bc764093e3500edff9b3ab5235
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-15 14:54:39 +0600

    Fix authentication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   include JsonWebToken
-
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_request
 
   protected
 
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation, :current_password)
     end
   end
 
