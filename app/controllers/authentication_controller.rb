274d49a34ead37460bcf0cdbaf5f847163f31de1
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-15 14:55:25 +0600

    Add comments methods

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,7 @@
 class AuthenticationController < ApplicationController
   protect_from_forgery with: :null_session
   skip_before_action :authenticate_request, only: [:login]
-  # POST /auth/login
+
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+  skip_before_action :authenticate_request
+  before_action :authenticate_request, only: [:add_comment]
+  protect_from_forgery with: :null_session, only: [:add_comment]
+
   def create
     @comment = current_user.comments.new(comment_params)
 
@@ -17,6 +21,26 @@ class CommentsController < ApplicationController
     @post.decrement!(:comments_counter)
     @comment.destroy!
     redirect_to user_post_path(id: @post.id), notice: 'Comment was successfully deleted!'
+  end
+
+  def comments
+    post = Post.find(params[:id])
+
+    respond_to do |format|
+      format.json { render json: post.comments }
+    end
+  end
+
+  def add_comment
+    comment = Comment.new(author: @curr_user, post_id: params[:post_id], text: params[:text])
+
+    respond_to do |format|
+      if comment.save
+        format.json { render json: comment }
+      else
+        format.json { render json: { success: false, message: comment.errors.full_messages } }
+      end
+    end
   end
 
   private
