ba018df6b80a5b219df47109bc2a18d4740c109c
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-15 16:48:43 +0600

    Add register action

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,13 @@
 class AuthenticationController < ApplicationController
   protect_from_forgery with: :null_session
-  skip_before_action :authenticate_request, only: [:login]
+  skip_before_action :authenticate_request, only: %i[login register]
+
+  def register
+    @user = User.create! name: params[:name], email: params[:email], password: params[:password], bio: params[:bio],
+                         photo: params[:photo], confirmed_at: Time.now
+
+    render json: { user: @user }, status: :ok
+  end
 
   def login
     @user = User.find_by_email(params[:email])
